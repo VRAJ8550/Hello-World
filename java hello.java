4cc73adeac14383d0ccc90d2e4e68e6995fe1673
Vraj Sharma <vrajsharma240@gmail.com>
2025-08-25 10:14:31 +0530

    Update java hello.java
    Add factorial function in Hello.java

--- java hello.java
+++ java hello.java
@@ -1,5 +1,13 @@
-public class Hello {
+public class Hello 
+static int factorial(int n) {
+    if (n == 0 || n == 1) return 1;
+    return n * factorial(n - 1);
+}
+{
     public static void main(String[] args) {
+        System.out.println("Factorial of 5: " + factorial(5));
+
         System.out.println("Hello, GitHub!");
     }
 }
+
